836f4fb8e2d7c1f2f38e11e047ffc422fc69603f
djordjijeK <krivokapic.djordjije@gmail.com>
2021-06-09 22:23:14 +0200

    throughput experiment started

---
+++ experiments/scripts/throughput.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# script options
+clean_build=True
+
+# execution options
+walk_model="deepwalk"             # deepwalk | node2vec
+paramP=0.2                        # node2vec paramP
+paramQ=0.7                        # node2vec paramQ
+sampler_init_strategy="weight"    # random | burnin | weight
+declare -a graphs=("email-graph")
+declare -a walks_per_node=(40 80)
+declare -a walk_length=(20 40)
+
+# convert graphs in adjacency graph format if necessary
+for graph in "${graphs[@]}"; do
+  FILE=../data/"${graph}".adj
+  if test -f "$FILE"; then
+      echo 'Skipping conversion of a graph ' "${graph[@]}" ' to adjacency graph format!'
+  else
+      echo 'Converting graph ' "${graph[@]}" ' to adjacency graph format ...'
+      ./../bin/SNAPtoAdj -s -f "../data/${graph}" "../data/${graph}.adj"
+      echo 'Graph ' "${graph[@]}" ' converted to adjacency graph format!'
+  fi
+done
+
+# create the build directory
+mkdir -p ../../cmake-build; cd ../../cmake-build; cmake -DCMAKE_BUILD_TYPE=Release ..; cd experiments;
+
+# build the throughput experiment
+make throughput
+
+# execute experiments
+for wpv in "${walks_per_node[@]}"; do
+    for wl in "${walk_length[@]}"; do
+        for graph in "${graphs[@]}"; do
+            printf "\n"
+            printf "Graph: ${graph} \n"
+            time ./throughput -s -f "data/${graph}.adj" -w "${wpv}" -l "${wl}" -model "${walk_model}" -paramP "${paramP}" -paramQ "${paramQ}" -init "${sampler_init_strategy}"
+        done
+    done
+done
+
+# clean build if necessary
+if [ "$clean_build" = True ] ; then
+    cd ../../;
+    rm -rf cmake-build;
+    rm experiments/data/*.adj
+fi
---
+++ experiments/src/throughput.cpp
@@ -0,0 +1,85 @@
+#include <dock.h>
+
+void throughput(commandLine& command_line)
+{
+    string fname             = string(command_line.getOptionValue("-f", default_file_name));
+    bool mmap                = command_line.getOption("-m");
+    bool is_symmetric        = command_line.getOption("-s");
+    bool compressed          = command_line.getOption("-c");
+    size_t n_parts           = command_line.getOptionLongValue("-nparts", 1);
+
+    size_t walks_per_vertex = command_line.getOptionLongValue("-w", config::walks_per_vertex);
+    size_t length_of_walks  = command_line.getOptionLongValue("-l", config::walk_length);
+    string model            = string(command_line.getOptionValue("-model", "deepwalk"));
+    double paramP           = command_line.getOptionDoubleValue("-paramP", config::paramP);
+    double paramQ           = command_line.getOptionDoubleValue("-paramQ", config::paramQ);
+    string init_strategy    = string(command_line.getOptionValue("-init", "weight"));
+
+    config::walks_per_vertex = walks_per_vertex;
+    config::walk_length      = length_of_walks;
+
+    std::cout << "Walks per vertex: " << (int) config::walks_per_vertex << std::endl;
+    std::cout << "Walk length: " <<  (int) config::walk_length << std::endl;
+
+    if (model == "deepwalk")
+    {
+        config::random_walk_model = types::RandomWalkModelType::DEEPWALK;
+
+        std::cout << "Walking model: DEEPWALK" << std::endl;
+    }
+    else if (model == "node2vec")
+    {
+        config::random_walk_model = types::RandomWalkModelType::NODE2VEC;
+        config::paramP = paramP;
+        config::paramQ = paramQ;
+
+        std::cout << "Walking model: NODE2VEC | Params (p,q) = " << "(" << config::paramP << "," << config::paramQ << ")" << std::endl;
+    }
+    else
+    {
+        std::cerr << "Unrecognized walking model! Abort" << std::endl;
+        std::exit(1);
+    }
+
+    if (init_strategy == "burnin")
+    {
+        config::sampler_init_strategy = types::SamplerInitStartegy::BURNIN;
+
+        std::cout << "Sampler strategy: BURNIN" << std::endl;
+    }
+    else if (init_strategy == "weight")
+    {
+        config::sampler_init_strategy = types::SamplerInitStartegy::WEIGHT;
+
+        std::cout << "Sampler strategy: WEIGHT" << std::endl;
+    }
+    else if (init_strategy == "random")
+    {
+        config::sampler_init_strategy = types::SamplerInitStartegy::RANDOM;
+
+        std::cout << "Sampler strategy: RANDOM" << std::endl;
+    }
+    else
+    {
+        std::cerr << "Unrecognized sampler init strategy" << std::endl;
+        std::exit(1);
+    }
+
+    size_t n;
+    size_t m;
+    uintE* offsets;
+    uintV* edges;
+    std::tie(n, m, offsets, edges) = read_unweighted_graph(fname.c_str(), is_symmetric, mmap);
+
+    dygrl::Dock dock = dygrl::Dock(n, m, offsets, edges);
+    dock.create_random_walks();
+}
+
+int main(int argc, char** argv)
+{
+    std::cout << "Running experiment with " << num_workers() << " threads." << std::endl;
+    commandLine command_line(argc, argv, "");
+
+    throughput(command_line);
+}
+
