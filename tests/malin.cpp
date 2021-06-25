8b6dcd04ea7813a7760e57dec7e5d1c82f4eda6b
djordjijeK <krivokapic.djordjije@gmail.com>
2021-06-25 17:08:00 +0200

    all clear

--- tests/malin.cpp
+++ tests/malin.cpp
@@ -15,7 +15,7 @@ class MalinTest : public testing::Test
         uintE* offsets;
         bool mmap = false;
         bool is_symmetric = true;
-        std::string default_file_path = "data/email-graph";
+        std::string default_file_path = "data/cora-graph";
 };
 
 void MalinTest::SetUp()
