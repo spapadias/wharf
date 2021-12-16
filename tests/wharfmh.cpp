585eb18762f7124a2b29bb7410bca176d36754b0
spapadias <s.papadias@protonmail.com>
2021-12-16 12:22:16 +0100

    test vs baseline-r4

--- tests/wharfmh.cpp
+++ tests/wharfmh.cpp
@@ -392,3 +392,131 @@ TEST_F(WharfMHTest, WharfMHThroughputLatency)
 
     std::cout << std::endl;
 }
+
+TEST_F(WharfMHTest, WharfMHThroughputLatency2)
+{
+	dygrl::WharfMH WharfMH = dygrl::WharfMH(total_vertices, total_edges, offsets, edges);
+	WharfMH.generate_initial_random_walks();
+	int n_trials = 1; //3;
+
+	cout << "WALK CORPUS" << endl;
+	for (auto i = 0; i < total_vertices; i++)
+		cout << WharfMH.walk(i) << endl;
+
+	auto batch_sizes = pbbs::sequence<size_t>(1);
+	batch_sizes[0] = 5;
+//	batch_sizes[1] = 50;
+//	batch_sizes[2] = 500;
+//	batch_sizes[3] = 5000;
+//	batch_sizes[4] = 50000;
+//	batch_sizes[5] = 500000;
+
+	for (short int i = 0; i < batch_sizes.size(); i++)
+	{
+		timer insert_timer("InsertTimer");
+		timer delete_timer("DeleteTimer");
+
+		graph_update_time_on_insert.reset();
+		walk_update_time_on_insert.reset();
+		graph_update_time_on_delete.reset();
+		walk_update_time_on_delete.reset();
+
+		std::cout << "Batch size = " << 2*batch_sizes[i] << " | ";
+
+		double last_insert_time = 0;
+		double last_delete_time = 0;
+
+		auto latency_insert = pbbs::sequence<double>(n_trials);
+		auto latency_delete = pbbs::sequence<double>(n_trials);
+		auto latency        = pbbs::sequence<double>(n_trials);
+
+		double total_insert_walks_affected = 0;
+		double total_delete_walks_affected = 0;
+
+		for (short int trial = 0; trial < n_trials; trial++)
+		{
+			size_t graph_size_pow2 = 1 << (pbbs::log2_up(total_vertices) - 1);
+			auto edges = utility::generate_batch_of_edges(batch_sizes[i], total_vertices, false, false);
+
+			std::cout << edges.second << " ";
+
+			insert_timer.start();
+			auto x = WharfMH.insert_edges_batch(edges.second, edges.first, false, true, graph_size_pow2);
+			insert_timer.stop();
+
+			total_insert_walks_affected += x.size();
+
+			last_insert_time = walk_update_time_on_insert.get_total() - last_insert_time;
+			latency_insert[trial] = last_insert_time / x.size();
+
+//			delete_timer.start();
+//			auto y = WharfMH.delete_edges_batch(edges.second, edges.first, false, true, graph_size_pow2);
+//			delete_timer.stop();
+
+//			total_delete_walks_affected += y.size();
+
+//			last_delete_time = walk_update_time_on_delete.get_total() - last_delete_time;
+//			latency_delete[trial] = last_delete_time / y.size();
+
+//			latency[trial] = (last_insert_time + last_delete_time) / (x.size() + y.size());
+			latency[trial] = latency_insert[trial];
+
+			// free edges
+			pbbs::free_array(edges.first);
+		}
+
+		std::cout << std::endl;
+
+		std::cout << "Average insert time = " << insert_timer.get_total() / n_trials << std::endl;
+		std::cout << "Average graph update insert time = " << graph_update_time_on_insert.get_total() / n_trials << std::endl;
+		std::cout << "Average walk update insert time = "
+		          << walk_update_time_on_insert.get_total() / n_trials
+		          << ", average walk affected = " << total_insert_walks_affected / n_trials << std::endl;
+
+//		std::cout << "Average delete time = " << delete_timer.get_total() / n_trials << std::endl;
+//		std::cout << "Average graph update delete time = " << graph_update_time_on_delete.get_total() / n_trials << std::endl;
+//		std::cout << "Average walk update delete time = "
+//		          << walk_update_time_on_delete.get_total() / n_trials
+//		          << ", average walk affected = " << total_delete_walks_affected / n_trials << std::endl;
+
+		std::cout << "Average walk insert latency = { ";
+		for(int i = 0; i < n_trials; i++)
+		{
+			std::cout << latency_insert[i] << " ";
+		}
+		std::cout << "}" << std::endl;
+
+//		std::cout << "Average walk delete latency = { ";
+//		for(int i = 0; i < n_trials; i++)
+//		{
+//			std::cout << latency_delete[i] << " ";
+//		}
+//		std::cout << "}" << std::endl;
+
+		std::cout << "Average walk update latency = { ";
+		for(int i = 0; i < n_trials; i++)
+		{
+			std::cout << latency[i] << " ";
+		}
+		std::cout << "}" << std::endl;
+	}
+
+	WharfMH.destroy_index();
+	timer generate_initial_walks("Generate Initial Random Walks", false);
+
+	for (int i = 0; i < n_trials; i++)
+	{
+		generate_initial_walks.start();
+		WharfMH.generate_initial_random_walks();
+		generate_initial_walks.stop();
+
+		WharfMH.destroy_index();
+	}
+
+
+	std::cout << std::endl
+	          << "Average time to generate random walks from scratch = "
+	          << generate_initial_walks.get_total() / n_trials << std::endl;
+
+	std::cout << std::endl;
+}
